463ce59a6b2cf067c56efc8981fd9e350f52acc5
MKrale <merlijnkrale@gmail.com>
2022-09-19 00:15:29 +0200

    Combined all shit & added Observer stuff
    This git is getting very messy guys, I don't think we need to make our own branches...

---
+++ Assignment_02/TCPChat_Decorator/BasicChat.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.net.*;
+
+public class BasicChat implements Chat {
+
+	@Override
+	public void Assemble() {
+		System.out.println("Basic chat!");
+	}
+
+}
