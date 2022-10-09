20120aa90dd2491d74176a2fd2c63d25007742f2
rroell <roelliie@hotmail.nl>
2022-10-09 19:30:48 +0200

    some deletion of old assignments

---
+++ Assignment_05/workspace/Assignment5_testFeatureModel/src/Main.java
@@ -0,0 +1,27 @@
+ class  Main {
+	
+    String more;
+
+	
+
+    Main  () { more = "Colour";  last = "EncryptionLast"; }
+
+	
+ 
+     private void  print__wrappee__Colour  () {
+        original();
+        System.out.println( more );
+    }
+
+	
+
+    void print() {
+    	print__wrappee__Colour();
+    	System.out.println( last );
+    }
+
+	
+    String last;
+
+
+}
